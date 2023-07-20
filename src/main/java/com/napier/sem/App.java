920d98589c85ca57639d7318067e069ae3e6c3a8
may <40614797@live.napier.ac.uk>
2023-07-20 12:45:02 +0630

    Testing Lab 08 - Maven Compile Error

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -2,40 +2,51 @@ package com.napier.sem;
 
 import java.sql.*;
 import java.util.*;
-
 public class App
 {
-    private Connection con = null;
     public static void main(String[] args)
+
     {
+        //Scanner sc = new Scanner(System.in);
+        //System.out.println("Enter the amount of employees to display");
+        //int limit = sc.nextInt();
+
+        int limit = 2;
+
+
+        Employee e = new Employee();
         // Create new Application
         App a = new App();
-        // Create new employee object
-        Employee e = new Employee();
-        // Connect to database
-        a.connect();
 
+        // Connect to database
+        //a.connect();
+        a.connect();
         // Extract employee salary information
         ArrayList<Employee> employees = e.getAllSalaries(a.con);
         e.printSalaries(employees);
 
+        // Test the size of the returned data - should be 240124
+        System.out.println(employees.size());
         // Get Employee
         //Employee emp = a.getEmployee(255530);
-        //Display results
+
+        // Display results
         //a.displayEmployee(emp);
 
         // Disconnect from database
         a.disconnect();
     }
-    /**
- * Connection to MySQL database.
- */
 
+
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
 
     /**
      * Connect to the MySQL database.
      */
-    public void connect( ) {
+    public void connect() {
         try {
             // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -51,22 +62,19 @@ public class App
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
-                //con = DriverManager.getConnection("jdbc:mysql://" + location
-                //                + "/employees?allowPublicKeyRetrieval=true&useSSL=false",
-                //        "root", "example");
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
-
+                // Connect to database in localhost
+                // con = DriverManager.getConnection("jdbc:mysql://localhost:33061/world", "root", "example");
                 System.out.println("Successfully connected");
                 break;
             } catch (SQLException sqle) {
-                System.out.println("Failed to connect to database attempt " +                                  Integer.toString(i));
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
             } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
     }
-
     /**
      * Disconnect from the MySQL database.
      */
@@ -94,14 +102,17 @@ public class App
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    "SELECT employees.emp_no, dept_manager.emp_no as manager_emp_id, employees.first_name, employees.last_name, titles.title, salaries.salary, departments.dept_name, titles.from_date, titles.to_date FROM dept_emp "
-                            + "INNER JOIN employees ON employees.emp_no = dept_emp.emp_no "
-                            + "INNER JOIN departments ON dept_emp.dept_no = departments.dept_no "
-                            + "INNER JOIN salaries ON employees.emp_no = salaries.emp_no "
-                            + "INNER JOIN titles ON employees.emp_no = titles.emp_no "
-                            + "INNER JOIN dept_manager ON employees.emp_no = dept_manager.emp_no "
-                            + "WHERE titles.to_date = '9999-01-01' "
-                            + "ORDER BY titles.from_date DESC";
+                    "SELECT dept_manager.emp_no as manager_emp_id, employees.first_name,"+
+                            "employees.last_name, employees.emp_no, titles.title, salaries.salary,"+
+                            "departments.dept_name, titles.from_date, titles.to_date "+
+                            "FROM dept_emp "+
+                            "JOIN employees ON employees.emp_no = dept_emp.emp_no "+
+                            "JOIN departments ON departments.dept_no = dept_emp.dept_no "+
+                            "JOIN salaries ON salaries.emp_no = employees.emp_no "+
+                            "JOIN titles ON titles.emp_no = employees.emp_no "+
+                            "JOIN dept_manager ON dept_manager.emp_no = employees.emp_no "+
+                            "WHERE titles.to_date = '9999-01-01' "+
+                            "ORDER BY titles.from_date DESC limit ";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Return new employee if valid.
@@ -112,10 +123,9 @@ public class App
                 emp.emp_no = rset.getInt("emp_no");
                 emp.first_name = rset.getString("first_name");
                 emp.last_name = rset.getString("last_name");
-                emp.salary = rset.getInt("salary");
                 emp.title = rset.getString("title");
+                emp.salary = rset.getInt("salary");
                 emp.dept_name = rset.getString("dept_name");
-                //emp.manager = rset.getString("manager");
                 return emp;
             }
             else
@@ -140,10 +150,9 @@ public class App
                             + emp.title + "\n"
                             + "Salary:" + emp.salary + "\n"
                             + emp.dept_name + "\n");
-                            //+ "Manager: " + emp.manager + "\n");
+            //+ "Manager: " + emp.manager + "\n");
         }
     }
-
     public void printSalaries(ArrayList<Employee> employees)
     {
         // Check employees is not null
